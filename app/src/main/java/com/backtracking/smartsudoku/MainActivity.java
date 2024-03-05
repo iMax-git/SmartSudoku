9d59821549a2b6b458aae9e6e68b75c32576e8b5
Jan De Rudder <39810873+janderudder@users.noreply.github.com>
2024-03-05 21:28:01 +0100

    add buttons to select test activity

--- app/src/main/java/com/backtracking/smartsudoku/MainActivity.java
+++ app/src/main/java/com/backtracking/smartsudoku/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,9 +12,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, TestGridViewActivity.class);
-        startActivity(intent);
+
+        Button btnGridLayout = findViewById(R.id.btnGridLayout);
+        btnGridLayout.setOnClickListener(v -> {
+            Intent intent = new Intent(this, GridLayoutActivity.class);
+            startActivity(intent);
+        });
+
+        Button btnTestGridView = findViewById(R.id.btnTestGridView);
+        btnTestGridView.setOnClickListener(v -> {
+            Intent intent = new Intent(this, TestGridViewActivity.class);
+            startActivity(intent);
+        });
     }
 }
