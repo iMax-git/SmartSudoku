db0301c8ebc66d3b4dbd94d73aabd97adde886a5
Jan De Rudder <39810873+janderudder@users.noreply.github.com>
2024-03-06 16:26:09 +0100

    wip

--- app/src/main/java/com/backtracking/smartsudoku/GameActivity.java
+++ app/src/main/java/com/backtracking/smartsudoku/GameActivity.java
@@ -4,16 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.backtracking.smartsudoku.models.Grid;
+
 import java.time.Clock;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Stack;
 
 
 public class GameActivity extends AppCompatActivity {
 
-    Clock clock;
+    static Clock clock = Clock.systemDefaultZone();
+    Stack<Grid> stateStack;
+    Stack<Grid> redoStateStack;
+    LocalDateTime timer;
 
     public GameActivity() {
-        this.clock = Clock.systemDefaultZone();
+        this.stateStack = new Stack<>();
+        this.redoStateStack = new Stack<>();
+        this.startNewGame();
+    }
+
+    public void startNewGame() {
+        this.stateStack.clear();
+        this.redoStateStack.clear();
+        this.timer = LocalDateTime.now();
     }
 
     @Override
