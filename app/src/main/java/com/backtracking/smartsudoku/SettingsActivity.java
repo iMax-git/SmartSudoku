ed9d5f5b46f060d7c6d725c85db8f05499babb6f
Jan De Rudder <39810873+janderudder@users.noreply.github.com>
2024-04-10 11:13:56 +0200

    add google login feature

--- app/src/main/java/com/backtracking/smartsudoku/SettingsActivity.java
+++ app/src/main/java/com/backtracking/smartsudoku/SettingsActivity.java
@@ -1,25 +1,46 @@
 package com.backtracking.smartsudoku;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.backtracking.smartsudoku.adapter.WallpaperAdapter;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
 
-
-
     private final List<String> wallpaperList = new ArrayList<>();
     private final List<String> wallpaperLabels = new ArrayList<>();
+
+    // Firebase Auth and Google login
+    private static final String TAG = "SettingsActivity";
+    private static final int RC_SIGN_IN = 9001;
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private GoogleSignInClient signInClient;
 
 
     @Override
@@ -80,18 +101,132 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
-
         ImageButton btnCloseSettings = findViewById(R.id.btnCloseSettings);
         btnCloseSettings.setOnClickListener(v -> {
             // Fermer l'activité
             finish();
         });
-        }
 
 
+        // auth and login
+
+        GoogleSignInOptions options =
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                        .build();
+
+        this.signInClient = GoogleSignIn.getClient(this, options);
+        findViewById(R.id.btnLoginGoogle).setOnClickListener(view -> signIn());
+
+        this.authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                SettingsActivity.this.onAuthStateChanged(firebaseAuth.getCurrentUser());
+            }
+        };
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.auth = FirebaseAuth.getInstance();
+        this.auth.addAuthStateListener(this.authStateListener);
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // detach listener to avoid a memory leak
+        if (authStateListener != null) {
+            auth.removeAuthStateListener(this.authStateListener);
+        }
+    }
+
+
+    private void signIn() {
+        Intent signInIntent = signInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // result from GoogleSignInClient.getSignInIntent(...);
+            if (resultCode == RESULT_OK) {
+                GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+                if (account != null) {
+                    firebaseAuthWithGoogle(account);
+                }
+            } else {
+                Log.w(TAG, "Google sign-in failed");
+                Toast.makeText(this, "Google sign-in failed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful())
+                    {
+                        FirebaseUser user = auth.getCurrentUser();
+
+                        if (user == null) {
+                            Toast.makeText(SettingsActivity.this, "User is NULL ERROR",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(SettingsActivity.this, "User authenticated: "
+                                            + ((user.getDisplayName()!=null) ? user.getDisplayName() : "<ERROR>"),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else { // sign in failed
+                        Log.w(TAG, "signInWithCredential:failure", task.getException());
+                        Toast.makeText(SettingsActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+    /**
+     * Callback on log-in log-off state change
+     * Update the activity display depending on whether the user is authenticated or not.
+     */
+    protected void onAuthStateChanged(@Nullable FirebaseUser user)
+    {
+        SignInButton signInButton = findViewById(R.id.btnLoginGoogle);
+        View loggedInView = findViewById(R.id.loggedInLayout);
+
+        if (user == null) {
+            loggedInView.setVisibility(View.GONE);
+            signInButton.setVisibility(View.VISIBLE);
+        } else {
+            signInButton.setVisibility(View.GONE);
+            loggedInView.setVisibility(View.VISIBLE);
+            TextView loggedInMessage = findViewById(R.id.loggedInStatusView);
+            String loggedInStatus = getString(R.string.loggedInStatusMessage, user.getDisplayName());
+            loggedInMessage.setText(loggedInStatus);
+        }
+    }
+
+
+    /**
+     * Call back for log-off button
+     */
+    public void logOff(View v) {
+        auth.signOut();
+    }
+}
 
